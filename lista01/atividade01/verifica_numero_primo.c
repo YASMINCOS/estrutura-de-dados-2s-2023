19646e70a1b2eac5235e88cddab3e227a04e4984
YASMIN VITORIA COSTA DE LIMA <yasmin.costa@a.ucb.br>
2023-08-12 10:41:17 -0300

    atividade 01 numeros primos

---
+++ lista01/atividade01/verifica_numero_primo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int verificaNumeroPrimo(int n){
+   int cont = 3;
+   
+   if (n%2 == 0){
+      return 0;
+   }
+
+   while (cont < n){
+     if (n % cont == 0){
+        return 0;
+     }
+     cont +=2;
+  }
+
+   return 1;
+}
+
+int main(int argc, char* argv[]){    
+    printf("%d", verificaNumeroPrimo(argc));
+    return 0;
+}
