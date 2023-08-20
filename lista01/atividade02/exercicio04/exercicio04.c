0bca81c9be6c0a825d10d69ad695917691392a6b
YASMINCOS <costayasmin367@gmail.com>
2023-08-20 10:08:32 -0300

    atividade 02

---
+++ lista01/atividade02/exercicio04/exercicio04.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int conta_int_array(int array[], int tamanho, int numero) {
+    int contador = 0;
+
+    for (int i = 0; i < tamanho; i++) {
+        if (array[i] == numero) {
+            contador++;
+        }
+    }
+
+    return contador;
+}
+
+int main() {
+    int array[] = {5, 7, 9, 6, 7, 11, 6, 5};
+    int tamanho = sizeof(array) / sizeof(array[0]);
+    int numero = 6;
+
+    int resultado = conta_int_array(array, tamanho, numero);
+
+    printf("O numero %d aparece no array %d vezes.\n", numero, resultado);
+
+    return 0;
+}
