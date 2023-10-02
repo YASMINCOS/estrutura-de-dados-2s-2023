b9a04ebe4eae8361a4260c641e794c2b56459acd
Pedro3148 <pedrohenrique3148@gmail.com>
2023-10-02 17:28:28 -0300

    PILHA

--- lista02/atividade07/pilha.h
+++ lista02/atividade07/pilha.h
@@ -1,9 +1,9 @@
+//coloquei um nome generico "Elemento" para apenas implementar as funções e struct. E as variaveis item e numero é o msm principio
 typedef struct Elemento {
     int numero;
     char item[50];
     struct Elemento *proximo_elemento;
 } Elemento;
-
 typedef struct Pilha{
     Elemento *topo;
     int qtd_elemento;
